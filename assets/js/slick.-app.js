75f0e527613c038bd1c42907210e1e6d6f19b74a
frsiam <siam.frs@gmail.com>
2024-02-20 16:15:08 +0600

    slick carousel add

---
+++ assets/js/slick.-app.js
@@ -0,0 +1,26 @@
+
+$('.my-slick-parent').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
