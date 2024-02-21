617b445b864bef31fbc07d764b071fe40b61a0fc
frsiam <siam.frs@gmail.com>
2024-02-21 12:42:19 +0600

    responsive mode

--- assets/js/slick.-app.js
+++ assets/js/slick.-app.js
@@ -3,6 +3,8 @@ $('.my-slick-parent').slick({
     centerMode: true,
     centerPadding: '60px',
     slidesToShow: 3,
+    prevArrow: '<button type="button" class="slick-perv btn btn-primary"><img src="../images/icon/map-pin-line.svg"/></button>',
+    nextArrow: '<button type="button" class="slick-next btn btn-info">N</button>',
     responsive: [
         {
             breakpoint: 768,
