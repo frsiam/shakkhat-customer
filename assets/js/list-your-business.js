29a2adca358bd13706488cd33ee569e80b55427e
frsiam <siam.frs@gmail.com>
2024-02-25 09:34:47 +0600

    add sick slider for customer review

---
+++ assets/js/list-your-business.js
@@ -0,0 +1,10 @@
+$('.review-slide').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    mobileFirst: true,
+});
